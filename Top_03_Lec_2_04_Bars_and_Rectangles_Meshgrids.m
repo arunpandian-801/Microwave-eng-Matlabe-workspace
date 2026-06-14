48d98322708e06f71758bae1ecc2007db44545ee
Arunpandian A <arun.pandian801@outlook.com>
2026-06-14 18:48:17 +0530

    Finished Bars and Rectangles using Meshgrid

--- Top_03_Lec_2_04_Bars_and_Rectangles_Meshgrids.m
+++ Top_03_Lec_2_04_Bars_and_Rectangles_Meshgrids.m
@@ -19,6 +19,58 @@ Sy = 10;
 Nx = 100;
 Ny = round((Nx/Sx)*Sy);
 
+% Computing Axes
+dx = Sx/Nx;
+dy = Sy/Ny;
+xa = (0.5:Nx - 0.5) * dx;
+ya = (0.5:Ny - 0.5) * dy;
+
+% Creating Grid
+[Y,X] = meshgrid(ya,xa);
+
+% Bar and Rectangle combined co-ordinates in physical dimensions
+x1 = 3.8;
+x2 = 5.3;
+y1 = 6;
+y2 = 8;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Visualize Bars
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Horizontal Bar
+A = (Y>=y1 & Y<=y2);
+subplot(2,2,1);
+imagesc(xa, ya, A.');
+axis equal tight;
+colorbar;
+title("Horizontal bar");
+
+% Vertical Bar
+A = (X>=x1 & X<=x2);
+subplot(2,2,2);
+imagesc(xa, ya, A.');
+axis equal tight;
+colorbar;
+title("Vertical bar");
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Visualize Rectangle
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Rectangle is formed by the intersection of both Horizontal bar and
+% Vertical bar
+
+% Of course you can use your own edge co-ordinates
+% But I got lazy and am just using the bar co-ordinates
+
+% Rectangle confined within x1, x2, y1, y2
+A = (X>=x1 & X<=x2 & Y>=y1 & Y<=y2);
+subplot(2,2,3);
+imagesc(xa, ya, A.');
+axis equal tight;
+colorbar;
+title("Rectangle");
 
 
 
